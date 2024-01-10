5ce0100d64c6060c30213a3f93c037fdefa818da
Mandy M. <127141046+mandaeliza@users.noreply.github.com>
2024-01-10 10:40:45 +0100

    Created factory function createPet

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+//Factory function and properties
+const createPet = function (name, species) {
+  const pet = {
+    name: name,
+    species: species,
+    isTired: 5,
+//Method for sleep
+    sleep: function () {
+        console.log(`${name} needs a nap. Zzz...`);
+        this.isTired = 1;
+    },
+//Property (method) for Playtime
+    play: function () {
+        if  (this.isTired  === 10) {
+            console.log("Too tired to play.");
+            this.sleep();
+        } else {
+            console.log(`Yay! ${this.name} loves to play!`);
+            this.isTired += 1;
+        }
+    }
+}
+    return pet;
+};
